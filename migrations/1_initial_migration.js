310e77ce2ce81627da5b460057debc44058141d9
DESKTOP-G7LN5EE\linhv <linhvocaothuy@gmail.com>
2018-04-07 18:09:20 +0700

    Add truffle configuration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+    deployer.deploy(Migrations);
+    deployer.deploy(Voting, ['Rama', 'Nick', 'Jose'], {gas: 6700000});
 };
